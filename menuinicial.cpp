763772234343c13633df91d82e78512d8e528bff
Emanuelle Costa <eme@vonlou.dev>
2018-02-06 15:28:34 -0300

    modificando a cor

--- menuinicial.cpp
+++ menuinicial.cpp
@@ -32,6 +32,7 @@ void exibeTabuleiro(char tabuleiro[10][10], char mascara[10][10])
 {
     // Vetor correspondente à uma cor.
     char blue[] = {0x1b, '1', '2', '3', '2', 'm', 0};
+    cout << blue;
 
     // Mapa indicador de colunas.
     int cont;
